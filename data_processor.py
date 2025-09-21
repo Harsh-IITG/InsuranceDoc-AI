1c6de9ddb891964acca2c1b1b7d2c95241b9e437
Harsh-IITG <agrawalharsh003@gmail.com>
2025-09-22 00:47:00 +0530

    Adding data processor module

---
+++ data_processor.py
@@ -0,0 +1,18 @@
+pc = Pinecone(api_key=PINECONE_API_KEY)
+import requests
+import fitz
+import textwrap
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+from pinecone import Pinecone, ServerlessSpec
+import hashlib
+import time
+
+# TODO: Implement document processing functions
+
+def main():
+    # TODO: Add main logic for testing data processor
+    pass
+
+# TODO: Add more functions and classes as needed
